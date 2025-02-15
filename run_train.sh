af29ec541c175a8ccf39cace3d8d1fd9f3bdc362
Nikolay Petrov Kormushev <nkormushev@gmail.com>
2025-02-15 12:27:28 +0100

    optimise cluster configs

--- run_train.sh
+++ run_train.sh
@@ -3,22 +3,20 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
-#SBATCH --time=2:00:00
+#SBATCH --time=20:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=20G
 #SBATCH --output=logs/train-%J.out
 #SBATCH --error=logs/train-%J.err
-#SBATCH --job-name="fcclip_train"
+#SBATCH --job-name="00003_fcclip_train"
 #SBATCH --constraint=h100
 
-module load CUDA
-nvidia-smi
+#module load CUDA
+#nvidia-smi
 
-cd fcclip/modeling/pixel_decoder/ops
-sh make.sh
-cd ../../../../
+#cd fcclip/modeling/pixel_decoder/ops
+#sh make.sh
+#cd ../../../../
 
-python3 ./train_net.py  --num-gpus 1 --config-file configs/coco/panoptic-segmentation/fcclip/fcclip_convnext_large_eval_ade20k.yaml \
-    SOLVER.IMS_PER_BATCH 4 SOLVER.BASE_LR 0.000025
-
- 
+python3 ./train_net.py  --num-gpus 1 --config-file configs/coco/panoptic-segmentation/fcclip/my_fcclip_convnext_large_eval_ade20k_r50.yaml \
+    SOLVER.IMS_PER_BATCH 12 SOLVER.BASE_LR 0.0003 OUTPUT_DIR LR_00003 MODEL.FC_CLIP.DIST_WEIGHT 1
