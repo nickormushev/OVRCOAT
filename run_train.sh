522e06505478215ef3bbc0a162e2b3c75a957648
Nikolay Petrov Kormushev <nkormushev@gmail.com>
2025-02-17 13:05:03 +0100

    rework testing config

--- run_train.sh
+++ run_train.sh
@@ -8,15 +8,12 @@
 #SBATCH --mem=20G
 #SBATCH --output=logs/train-%J.out
 #SBATCH --error=logs/train-%J.err
-#SBATCH --job-name="00003_fcclip_train"
+#SBATCH --job-name="dim_red_fcclip_train"
 #SBATCH --constraint=h100
-
-#module load CUDA
-#nvidia-smi
 
 #cd fcclip/modeling/pixel_decoder/ops
 #sh make.sh
 #cd ../../../../
 
 python3 ./train_net.py  --num-gpus 1 --config-file configs/coco/panoptic-segmentation/fcclip/my_fcclip_convnext_large_eval_ade20k_r50.yaml \
-    SOLVER.IMS_PER_BATCH 12 SOLVER.BASE_LR 0.0003 OUTPUT_DIR LR_00003 MODEL.FC_CLIP.DIST_WEIGHT 1
+    SOLVER.IMS_PER_BATCH 12 SOLVER.BASE_LR 0.0001 OUTPUT_DIR DIM_REDUCE MODEL.FC_CLIP.DIST_WEIGHT 1 MODEL.FC_CLIP.CE_WEIGHT 50
