4715598907e34325e9cf30f96ffaf9c12f8f4f78
nickormushev <nkormushev@gmail.com>
2025-05-26 22:36:53 +0200

    update create_val_set script

--- create_val_set.py
+++ create_val_set.py
@@ -5,7 +5,7 @@ with open('./datasets/ADEChallengeData2016/ade20k_panoptic_train_real.json', 'r'
     val_data = json.load(f)
 
 
-val_data['images'] = val_data['images'][0:2000]
-val_data['annotations'] = val_data['annotations'][0:2000]
+val_data['images'] = val_data['images'][0:5000]
+val_data['annotations'] = val_data['annotations'][0:5000]
 
-json.dump(val_data, open('./datasets/ADEChallengeData2016/ade20k_panoptic_val_2000.json', 'w'), indent=4)
+json.dump(val_data, open('./datasets/ADEChallengeData2016/ade20k_panoptic_val_5000.json', 'w'), indent=4)
